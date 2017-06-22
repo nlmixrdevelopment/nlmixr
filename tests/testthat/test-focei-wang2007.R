1a755c45e8a0182a09259fb298f45c73c9a14ba1
Fidler <matthew.fidler@gmail.com>
2017-06-22 13:42:13 -0500

    Fix FOCEI test

--- tests/testthat/test-focei-wang2007.R
+++ tests/testthat/test-focei-wang2007.R
@@ -1,5 +1,6 @@
 context("Test FOCEI with NONMEM")
 rxPermissive({
+
     mypar1 = function ()
     {
         ke = theta[1] * exp(eta[1]);
@@ -34,7 +35,7 @@ rxPermissive({
     fit.add.prop <- focei.fit(data=dat, inits, mypar1, model=mod,pred=pred, optim="bobyqa", control=list(NOTRUN=TRUE))
 
     test_that("Matches NONMEM objective function; (Based on Wang2007)", {
-        expect_equal(round(fit$objective, 3), 39.458); # Matches Table 2 Prop FOCEI for NONMEM
+        expect_equal(round(fit.prop$objective, 3), 39.458); # Matches Table 2 Prop FOCEI for NONMEM
         expect_equal(round(fit.add$objective, 3), -2.059); # Matches Table 2 Add FOCE=FOCEI for NONMEM
         expect_equal(round(fit.add.prop$objective, 3), 51.882); # New entry that does not match Wang2007
     })
