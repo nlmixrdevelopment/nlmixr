67efae01c9931641f5f2825cd10c91e2a0577d66
Fidler <matthew.fidler@gmail.com>
2017-07-20 17:38:53 -0500

    Save Bolus_1CPT dataset in nlmixr package

--- tests/testthat/test-model04.R
+++ tests/testthat/test-model04.R
@@ -4,25 +4,23 @@ library(nlmixr)
 context("NLME04: one-compartment bolus, multiple-dose")
 
 if (identical(Sys.getenv("NLMIXR_VALIDATION"), "true")) {
-  
+
   test_that("Closed-form", {
-    datr <- read.csv("Bolus_1CPT.csv",
-                     header = TRUE,
-                     stringsAsFactors = F)
+    datr <- Bolus_1CPT
     datr$EVID <- ifelse(datr$EVID == 1, 101, datr$EVID)
     datr <- datr[datr$EVID != 2,]
-    
+
     specs1 <-
       list(
         fixed = lCL + lV ~ 1,
         random = pdDiag(lCL + lV ~ 1),
         start = c(lCL = 1.6, lV = 4.5)
       )
-    
+
     runno <- "N004"
-    
+
     dat <- datr
-    
+
     fit <-
       nlme_lin_cmpt(
         dat,
@@ -32,50 +30,48 @@ if (identical(Sys.getenv("NLMIXR_VALIDATION"), "true")) {
         oral = FALSE,
         weight = varPower(fixed = c(1))
       )
-    
+
     z <- summary(fit)
-    
+
     expect_equal(signif(as.numeric(fit$logLik),6), -38528.5)
     expect_equal(signif(AIC(fit), 6), 77067)
-    expect_equal(signif(BIC(fit), 6), 77101.2)   
-    
+    expect_equal(signif(BIC(fit), 6), 77101.2)
+
     expect_equal(signif(as.numeric(fit$coefficients$fixed[1]),3), 1.36)
     expect_equal(signif(as.numeric(fit$coefficients$fixed[2]),3), 4.20)
-    
+
     expect_equal(as.numeric(signif(exp(attr(z$apVar, "Pars"))[1], 3)), 0.270)
     expect_equal(as.numeric(signif(exp(attr(z$apVar, "Pars"))[2], 3)), 0.310)
     expect_equal(as.numeric(signif(exp(attr(z$apVar, "Pars"))[3], 3)), 0.203)
-    
+
   })
-  
+
   test_that("ODE", {
-    datr <- read.csv("Bolus_1CPT.csv",
-                     header = TRUE,
-                     stringsAsFactors = F)
+    datr <- Bolus_1CPT
     datr$EVID <- ifelse(datr$EVID == 1, 101, datr$EVID)
     datr <- datr[datr$EVID != 2, ]
-    
+
     specs1 <-
       list(
         fixed = lCL + lV ~ 1,
         random = pdDiag(lCL + lV ~ 1),
         start = c(lCL = 1.6, lV = 4.5)
       )
-    
+
     runno <- "N004"
-    
+
     dat <- datr
-    
+
     ode1 <- "
     d/dt(centr)  = -(CL/V)*centr;
     "
-    
+
     mypar1 <- function(lCL, lV)
     {
       CL <- exp(lCL)
       V <- exp(lV)
     }
-    
+
     fitODE <-
       nlme_ode(
         dat,
@@ -88,20 +84,20 @@ if (identical(Sys.getenv("NLMIXR_VALIDATION"), "true")) {
         weight = varPower(fixed = c(1)),
         control = nlmeControl(pnlsTol = .01, msVerbose = TRUE)
       )
-    
+
     z <- summary(fitODE)
-    
+
     expect_equal(signif(as.numeric(fitODE$logLik),6), -38528.5)
     expect_equal(signif(AIC(fitODE), 6), 77066.9)
-    expect_equal(signif(BIC(fitODE), 6), 77101.2)   
-    
+    expect_equal(signif(BIC(fitODE), 6), 77101.2)
+
     expect_equal(signif(as.numeric(fitODE$coefficients$fixed[1]),3), 1.36)
     expect_equal(signif(as.numeric(fitODE$coefficients$fixed[2]),3), 4.20)
-    
+
     expect_equal(as.numeric(signif(exp(attr(z$apVar, "Pars"))[1], 3)), 0.270)
     expect_equal(as.numeric(signif(exp(attr(z$apVar, "Pars"))[2], 3)), 0.310)
     expect_equal(as.numeric(signif(exp(attr(z$apVar, "Pars"))[3], 3)), 0.203)
-    
-    
+
+
   })
 }
