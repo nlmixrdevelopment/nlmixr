519920e74266baca6fd0494da50e6f2d590fb636
kiran <kiranmai.bannu@gmail.com>
2018-11-20 14:34:57 +0530

    Test formatting update for Model 04

---
+++ tests/testthat/models/model_N004.R
@@ -0,0 +1,36 @@
+source("tests/testthat/models/helper-prep_fit.R")
+context("NLME04: one-compartment bolus, multiple-dose")
+runno <- "N004"
+
+datr <- Bolus_1CPT
+datr$EVID <- ifelse(datr$EVID == 1, 101, datr$EVID)
+datr <- datr[datr$EVID != 2,]
+
+specs1 <-
+  list(
+    fixed = lCL + lV ~ 1,
+    random = pdDiag(lCL + lV ~ 1),
+    start = c(lCL = 1.6, lV = 4.5)
+  )
+
+dat <- datr
+
+fit[[runno]] <-
+  nlme_lin_cmpt(
+    dat,
+    par_model = specs1,
+    ncmt = 1,
+    oral = FALSE,
+    weight = varPower(fixed = c(1)),
+    verbose = verbose_minimization,
+    control = default_control
+  )
+
+# Generate this with generate_expected_values(fit[[runno]])
+expected_values <-
+  list(
+    lik=c(-38528.49, 77066.98, 77101.21),
+    param=c(1.3613, 4.2008),
+    stdev_param=c(0.26971, 0.30991),
+    sigma=0.20304
+  )
