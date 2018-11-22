158965056cb068c72593a3dcf435ef750c0f02be
kiran <kiranmai.bannu@gmail.com>
2018-11-22 17:42:58 +0530

    Test formatting update for model 09

---
+++ tests/testthat/models/model_N009_ode.R
@@ -0,0 +1,47 @@
+source("tests/testthat/models/helper-prep_fit.R")
+context("NLME09: one-compartment bolus, Michaelis-Menten, single-dose")
+runno <- "N009_ode"
+
+ode1MM <- "d/dt(centr)  = -(VM*centr/V)/(KM+centr/V);"
+
+mypar3 <- function(lVM, lKM, lV)
+{
+  VM <- exp(lVM)
+  KM <- exp(lKM)
+  V <- exp(lV)
+}
+specs3 <-
+  list(
+    fixed = lVM + lKM + lV ~ 1,
+    random = pdDiag(lVM + lKM + lV ~ 1),
+    start = c(lVM = 7, lKM = 6, lV = 4)
+  )
+
+datr <- Bolus_1CPTMM
+datr$EVID <- ifelse(datr$EVID == 1, 101, datr$EVID)
+
+datr <- datr[datr$EVID != 2,]
+
+dat <- datr[datr$SD == 1,]
+
+fit[[runno]] <-
+  nlme_ode(
+    dat,
+    model = ode1MM,
+    par_model = specs3,
+    par_trans = mypar3,
+    response = "centr",
+    response.scaler = "V",
+    weight = varPower(fixed = c(1)),
+    verbose = verbose_minimization,
+    control = default_control
+  )
+
+# Generate this with generate_expected_values(fit[[runno]])
+expected_values <-
+  list(
+    lik=c(-13285.23, 26580.47, 26609.12),
+    param=c(1.4221, 4.3410),
+    stdev_param=c(0.84730, 0),
+    sigma=0.43887
+  )
