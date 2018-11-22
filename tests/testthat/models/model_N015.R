bd1c1cc905863b041de56fc39bd92c373e11777a
kiran <kiranmai.bannu@gmail.com>
2018-11-22 17:55:42 +0530

    Test formatting update for Model 15

---
+++ tests/testthat/models/model_N015.R
@@ -0,0 +1,49 @@
+source("tests/testthat/models/helper-prep_fit.R")
+context("NLME15: one-compartment infusion, multiple-dose")
+runno <- "N015"
+
+datr <-
+  read.csv("Infusion_1CPT.csv",
+           header = TRUE,
+           stringsAsFactors = F)
+
+datr$EVID <- ifelse(datr$EVID == 1, 10101, datr$EVID)
+
+datr <- datr[datr$EVID != 2, ]
+
+datIV <- datr[datr$AMT > 0, ]
+datIV$TIME <- datIV$TIME + (datIV$AMT / datIV$RATE)
+datIV$AMT  <- -1 * datIV$AMT
+
+datr <- rbind(datr, datIV)
+datr <- datr[order(datr$ID, datr$TIME), ]
+
+specs1 <-
+  list(
+    fixed = lCL + lV ~ 1,
+    random = pdDiag(lCL + lV ~ 1),
+    start = c(lCL = 1.5, lV = 4)
+  )
+
+dat <- datr
+
+fit[[runno]] <-
+  nlme_lin_cmpt(
+    dat,
+    par_model = specs1,
+    ncmt = 1,
+    oral = FALSE,
+    infusion = TRUE,
+    weight = varPower(fixed = c(1)),
+    verbose = verbose_minimization,
+    control = default_control
+  )
+
+# Generate this with generate_expected_values(fit[[runno]])
+expected_values <-
+  list(
+    lik=c(-37860.81, 75731.62, 75765.86),
+    param=c(1.3869, 4.2689),
+    stdev_param=c(0.28027, 0.30494),
+    sigma=0.20048
+  )
