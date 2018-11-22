a5147a16919086a1d8d00359390dc7166de5f687
kiran <kiranmai.bannu@gmail.com>
2018-11-22 18:09:41 +0530

    Test formatting update for Model 33

---
+++ tests/testthat/models/model_N033.R
@@ -0,0 +1,40 @@
+source("tests/testthat/models/helper-prep_fit.R")
+context("NLME33: two-compartment bolus, multiple-dose")
+runno <- "N033"
+
+datr <- Bolus_2CPT
+datr$EVID <- ifelse(datr$EVID == 1, 101, datr$EVID)
+datr <- datr[datr$EVID != 2,]
+specs6 <-
+  list(
+    fixed = lCL + lV + lCLD + lVT ~ 1,
+    random = pdDiag(lCL + lV + lCLD + lVT ~ 1),
+    start = c(
+      lCL = 1.37,
+      lV = 4.19,
+      lCLD = 1.37,
+      lVT = 3.87
+    )
+  )
+
+dat <- datr[datr$SD == 0,]
+
+fit[[runno]] <-
+  nlme_lin_cmpt(
+    dat,
+    par_model = specs6,
+    ncmt = 2,
+    oral = FALSE,
+    weight = varPower(fixed = c(1)),
+    verbose = verbose_minimization,
+    control = default_control
+  )
+
+# Generate this with generate_expected_values(fit[[runno]])
+expected_values <-
+  list(
+    lik=c(-27501.23, 55020.46, 55078.52),
+    param=c(1.3326, 4.1936, 1.29, 3.8091),
+    stdev_param=c(0.34247, 0.30254, 0.00067117, 0.27539),
+    sigma=0.20531
+  )
---
+++ tests/testthat/models/model_N033_ode.R
@@ -0,0 +1,60 @@
+source("tests/testthat/models/helper-prep_fit.R")
+context("NLME33: two-compartment bolus, multiple-dose")
+runno <- "N033_ode"
+
+datr <- Bolus_2CPT
+
+datr$EVID <- ifelse(datr$EVID == 1, 101, datr$EVID)
+datr <- datr[datr$EVID != 2,]
+
+ode2 <- "
+d/dt(centr)  = K21*periph-K12*centr-K10*centr;
+d/dt(periph) =-K21*periph+K12*centr;
+"
+
+mypar6 <- function(lCL, lV, lCLD, lVT)
+{
+  CL <- exp(lCL)
+  V  <- exp(lV)
+  CLD <- exp(lCLD)
+  VT <- exp(lVT)
+  K10 <- CL / V
+  K12 <- CLD / V
+  K21 <- CLD / VT
+}
+
+specs6 <-
+  list(
+    fixed = lCL + lV + lCLD + lVT ~ 1,
+    random = pdDiag(lCL + lV + lCLD + lVT ~ 1),
+    start = c(
+      lCL = 1.3,
+      lV = 4.19,
+      lCLD = 1.5,
+      lVT = 3.9
+    )
+  )
+
+dat <- datr[datr$SD == 0,]
+
+fit[[runno]] <-
+  nlme_ode(
+    dat,
+    model = ode2,
+    par_model = specs6,
+    par_trans = mypar6,
+    response = "centr",
+    response.scaler = "V",
+    weight = varPower(fixed = c(1)),
+    verbose = verbose_minimization,
+    control = default_control
+  )
+
+# Generate this with generate_expected_values(fit[[runno]])
+expected_values <-
+  list(
+    lik=c(-13285.23, 26580.47, 26609.12),
+    param=c(1.4221, 4.3410),
+    stdev_param=c(0.84730, 0),
+    sigma=0.43887
+  )
