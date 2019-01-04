da1491a6db7ed5823f1dc064ac4a9c52b47d4589
Fidler <matthew.fidler@gmail.com>
2019-01-04 01:00:42 -0600

    Add 001 nlme

---
+++ tests/testthat/models/values-foceU001ode-windows.R
@@ -0,0 +1,7 @@
+expected_values[[runno]] <-
+  list(
+    lik=c(-39669.28, 79348.56, 79382.79),
+    param=c(1.3533, 4.3983, 0.30859),
+    stdev_param=c(0.0074312, 0.00033028, NA),
+    sigma=c(0.30859)
+  )
---
+++ tests/testthat/models/values-saemU001ode-windows.R
@@ -0,0 +1,7 @@
+expected_values[[runno]] <-
+  list(
+    lik=c(-38529.4, 77068.79, 77103.03),
+    param=c(1.3601, 4.2019, 0.20249),
+    stdev_param=c(0.024713, 0.028347, NA),
+    sigma=c(0.20249)
+  )
