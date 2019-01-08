6fabb413025411c9917f043b72bcadf5fe89ab48
Fidler <matthew.fidler@gmail.com>
2019-01-08 10:30:32 -0600

    Add UI 002

---
+++ tests/testthat/models/values-foU002ode-windows.R
@@ -0,0 +1,7 @@
+expected_values[[runno]] <-
+  list(
+    lik=c(-34340.22, 68690.44, 68724.67),
+    param=c(1.249, 4.3228, 0.23581),
+    stdev_param=c(0.031472, 0.032859, NA),
+    sigma=c(0.23581)
+  )
