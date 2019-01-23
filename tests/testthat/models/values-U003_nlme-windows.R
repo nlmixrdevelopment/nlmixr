a3071129bc95d468addfa709175cf4b947f46fa2
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-01-23 13:01:17 -0600

    Update nlme fits again

--- tests/testthat/models/values-U003_nlme-windows.R
+++
@@ -1,7 +0,0 @@
-expected_values[[runno]] <-
-  list(
-    lik=c(-21468.41, 42946.83, 42975.48),
-    param=c(1.3585, 4.1934, -14.022),
-    stdev_param=c(1.5912, 1.3479, 1),
-    sigma=c(0.20027)
-  )
