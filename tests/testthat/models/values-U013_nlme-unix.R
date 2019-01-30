aa41b8533be1230176e0041a987a0fff5461f2fc
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2019-01-29 20:57:13 -0600

    Merge with master

--- tests/testthat/models/values-U013_nlme-unix.R
+++ tests/testthat/models/values-U013_nlme-unix.R
@@ -1,7 +1,12 @@
 expected_values[[runno]] <-
   list(
+<<<<<<< Updated upstream
     lik=c(-20670.28, 41350.56, 41379.21),
     param=c(1.4393, 4.3724, -16.353),
+=======
+    lik=c(-12853.7, 25717.39, 25746.04),
+    param=c(1.4393, 4.3724, 0.30942),
+>>>>>>> Stashed changes
     stdev_param=c(0.30623, 0.27788, 0.30942),
     sigma=c(0.30942)
   )
