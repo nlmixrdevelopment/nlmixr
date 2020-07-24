6a7a531ca032dd9eedc456501d2eadfbafe29023
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-07-24 15:03:09 -0500

    Enclose test-bootstrap in rxPermissive() so it isn't so loud

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -1,159 +1,163 @@
-context('bootstrap')
-samp_dat = theo_sd
+rxPermissive({
+  context('bootstrap')
 
-testthat::test_that("sampling should return different datasets at each call", {
-  a <- digest::digest(nlmixr:::sampling(samp_dat))
-  b <- digest::digest(nlmixr:::sampling(samp_dat))
-  testthat::expect_false(isTRUE(all.equal(a, b)))
-})
+  samp_dat = theo_sd
+
+  test_that("sampling should return different datasets at each call", {
+    a <- digest::digest(nlmixr:::sampling(samp_dat))
+    b <- digest::digest(nlmixr:::sampling(samp_dat))
+    testthat::expect_false(isTRUE(all.equal(a, b)))
+  })
 
 
-testthat::test_that("resuming the fit should not return the same datasets as before",
-                    {
-                      one.cmt <- function() {
-                        ini({
-                          ## You may label each parameter with a comment
-                          tka <- 0.45 # Log Ka
-                          tcl <- 1 # Log Cl
-                          ## This works with interactive models
-                          ## You may also label the preceding line with label("label text")
-                          tv <- 3.45
-                          label("log V")
-                          ## the label("Label name") works with all models
-                          eta.ka ~ 0.6
-                          eta.cl ~ 0.3
-                          eta.v ~ 0.1
-                          add.sd <- 0.7
-                        })
-                        model({
-                          ka <- exp(tka + eta.ka)
-                          cl <- exp(tcl + eta.cl)
-                          v <- exp(tv + eta.v)
-                          linCmt() ~ add(add.sd)
-                        })
-                      }
+  test_that("resuming the fit should not return the same datasets as before", {
 
-                      fit <- suppressWarnings(nlmixr(
-                        one.cmt,
-                        samp_dat,
-                        est = "focei",
-                        control = list(print = 0),
-                        table = list(npde = TRUE, cwres = TRUE)
-                      ))
-                      
+    one.cmt <- function() {
+      ini({
+        ## You may label each parameter with a comment
+        tka <- 0.45 # Log Ka
+        tcl <- 1 # Log Cl
+        ## This works with interactive models
+        ## You may also label the preceding line with label("label text")
+        tv <- 3.45
+        label("log V")
+        ## the label("Label name") works with all models
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl)
+        v <- exp(tv + eta.v)
+        linCmt() ~ add(add.sd)
+      })
+    }
 
-                      fit1 <- nlmixr:::bootstrapFit(fit, nboot = 2, restart = TRUE)
-                      fit2 <- nlmixr:::bootstrapFit(fit, nboot = 4, restart = FALSE)
-                      
-                      output_dir <-
-                        paste0("nlmixrBootstrapCache_",'fit', '_' ,fit$bootstrapMd5)
+    fit <- suppressWarnings(nlmixr(
+      one.cmt,
+      samp_dat,
+      est = "focei",
+      control = list(print = 0),
+      table = list(npde = TRUE, cwres = TRUE)
+    ))
 
-                      fnameBootDataPattern <- paste0('boot_data',
-                                                     "_", "[0-9]+", ".RData",
-                                                     sep = "")
-                      fileExists <- list.files(paste0("./", output_dir), pattern = fnameBootDataPattern)
 
-                      print(output_dir)
+    fit1 <- nlmixr:::bootstrapFit(fit, nboot = 2, restart = TRUE)
+    fit2 <- nlmixr:::bootstrapFit(fit, nboot = 4, restart = FALSE)
 
-                      fitdata <- lapply(fileExists, function(x) {
-                        readRDS(paste0("./", output_dir, "/", x, sep = ""))
-                      })
-                      
-                      a <- digest::digest(fitdata[[1]])
-                      b <- digest::digest(fitdata[[3]])
-                      testthat::expect_false(isTRUE(all.equal(a, b)))
+    output_dir <-
+      paste0("nlmixrBootstrapCache_",'fit', '_' ,fit$bootstrapMd5)
 
-                      a <- digest::digest(fitdata[[2]])
-                      b <- digest::digest(fitdata[[4]])
-                      testthat::expect_false(isTRUE(all.equal(a, b)))
-                      
-                      unlink(output_dir, recursive = TRUE, force = TRUE)
+    fnameBootDataPattern <- paste0('boot_data',
+                                   "_", "[0-9]+", ".RData",
+                                   sep = "")
+    fileExists <- list.files(paste0("./", output_dir), pattern = fnameBootDataPattern)
 
-                    })
+    ## print(output_dir)
 
-testthat::test_that("different confidence levels should result in different bands", {
-  one.cmt <- function() {
-    ini({
-      ## You may label each parameter with a comment
-      tka <- 0.45 # Log Ka
-      tcl <- 1 # Log Cl
-      ## This works with interactive models
-      ## You may also label the preceding line with label("label text")
-      tv <- 3.45
-      label("log V")
-      ## the label("Label name") works with all models
-      eta.ka ~ 0.6
-      eta.cl ~ 0.3
-      eta.v ~ 0.1
-      add.sd <- 0.7
+    fitdata <- lapply(fileExists, function(x) {
+      readRDS(paste0("./", output_dir, "/", x, sep = ""))
     })
-    model({
-      ka <- exp(tka + eta.ka)
-      cl <- exp(tcl + eta.cl)
-      v <- exp(tv + eta.v)
-      linCmt() ~ add(add.sd)
-    })
-  }
 
-  fit <- suppressWarnings(nlmixr(
-    one.cmt,
-    samp_dat,
-    est = "focei",
-    control = list(print = 0),
-    table = list(npde = TRUE, cwres = TRUE)
-  ))
+    a <- digest::digest(fitdata[[1]])
+    b <- digest::digest(fitdata[[3]])
+    expect_false(isTRUE(all.equal(a, b)))
 
-  fitlist <- modelBootstrap(fit, nboot = 4, restart = TRUE)
-  bootSummary1 <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.95)
-  bootSummary2 <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.75)
+    a <- digest::digest(fitdata[[2]])
+    b <- digest::digest(fitdata[[4]])
+    expect_false(isTRUE(all.equal(a, b)))
 
-  a <- digest::digest(bootSummary1$parFixedDf$confLower)
-  b <- digest::digest(bootSummary2$parFixedDf$confLower)
-  testthat::expect_false(isTRUE(all.equal(a, b)))
+    unlink(output_dir, recursive = TRUE, force = TRUE)
 
-  a <- digest::digest(bootSummary1$parFixedDf$confUpper)
-  b <- digest::digest(bootSummary2$parFixedDf$confUpper)
-  testthat::expect_false(isTRUE(all.equal(a, b)))
-})
+  })
 
-testthat::test_that("expected columns in fit$parFixedDf object should match", {
-  one.cmt <- function() {
-    ini({
-      ## You may label each parameter with a comment
-      tka <- 0.45 # Log Ka
-      tcl <- 1 # Log Cl
-      ## This works with interactive models
-      ## You may also label the preceding line with label("label text")
-      tv <- 3.45
-      label("log V")
-      ## the label("Label name") works with all models
-      eta.ka ~ 0.6
-      eta.cl ~ 0.3
-      eta.v ~ 0.1
-      add.sd <- 0.7
-    })
-    model({
-      ka <- exp(tka + eta.ka)
-      cl <- exp(tcl + eta.cl)
-      v <- exp(tv + eta.v)
-      linCmt() ~ add(add.sd)
-    })
-  }
+  testthat::test_that("different confidence levels should result in different bands", {
+    one.cmt <- function() {
+      ini({
+        ## You may label each parameter with a comment
+        tka <- 0.45 # Log Ka
+        tcl <- 1 # Log Cl
+        ## This works with interactive models
+        ## You may also label the preceding line with label("label text")
+        tv <- 3.45
+        label("log V")
+        ## the label("Label name") works with all models
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl)
+        v <- exp(tv + eta.v)
+        linCmt() ~ add(add.sd)
+      })
+    }
 
-  fit <- suppressWarnings(nlmixr(
-    one.cmt,
-    samp_dat,
-    est = "focei",
-    control = list(print = 0),
-    table = list(npde = TRUE, cwres = TRUE)
-  ))
+    fit <- suppressWarnings(nlmixr(
+      one.cmt,
+      samp_dat,
+      est = "focei",
+      control = list(print = 0),
+      table = list(npde = TRUE, cwres = TRUE)
+    ))
 
-  colsBefore = colnames(fit$parFixedDf)
-  fitlist = modelBootstrap(fit, nboot = 4, restart = TRUE)
+    fitlist <- modelBootstrap(fit, nboot = 4, restart = TRUE)
+    bootSummary1 <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.95)
+    bootSummary2 <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.75)
 
-  bootSummary <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.95)
+    a <- digest::digest(bootSummary1$parFixedDf$confLower)
+    b <- digest::digest(bootSummary2$parFixedDf$confLower)
+    testthat::expect_false(isTRUE(all.equal(a, b)))
 
-  colsAfter = colnames(fit$parFixedDf)
+    a <- digest::digest(bootSummary1$parFixedDf$confUpper)
+    b <- digest::digest(bootSummary2$parFixedDf$confUpper)
+    testthat::expect_false(isTRUE(all.equal(a, b)))
+  })
 
-  testthat::expect_equal(colsAfter, colsBefore)
-})
+  testthat::test_that("expected columns in fit$parFixedDf object should match", {
+    one.cmt <- function() {
+      ini({
+        ## You may label each parameter with a comment
+        tka <- 0.45 # Log Ka
+        tcl <- 1 # Log Cl
+        ## This works with interactive models
+        ## You may also label the preceding line with label("label text")
+        tv <- 3.45
+        label("log V")
+        ## the label("Label name") works with all models
+        eta.ka ~ 0.6
+        eta.cl ~ 0.3
+        eta.v ~ 0.1
+        add.sd <- 0.7
+      })
+      model({
+        ka <- exp(tka + eta.ka)
+        cl <- exp(tcl + eta.cl)
+        v <- exp(tv + eta.v)
+        linCmt() ~ add(add.sd)
+      })
+    }
+
+    fit <- suppressWarnings(nlmixr(
+      one.cmt,
+      samp_dat,
+      est = "focei",
+      control = list(print = 0),
+      table = list(npde = TRUE, cwres = TRUE)
+    ))
+
+    colsBefore = colnames(fit$parFixedDf)
+    fitlist = modelBootstrap(fit, nboot = 4, restart = TRUE)
+
+    bootSummary <- nlmixr:::getBootstrapSummary(fitlist, ci = 0.95)
+
+    colsAfter = colnames(fit$parFixedDf)
+
+    testthat::expect_equal(colsAfter, colsBefore)
+  })
+
+}, silent=TRUE)
