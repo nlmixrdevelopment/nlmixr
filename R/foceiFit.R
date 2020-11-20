6cf243ba797707953a7a019cb2c91d5b80821547
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-11-19 18:40:23 -0600

    Fix .cls for population mean modeling

--- R/foceiFit.R
+++ R/foceiFit.R
@@ -2330,6 +2330,7 @@ foceiFit.data.frame0 <- function(data,
     drop <- c(drop, "rxLambda", "rxYj")
     .w <- -which(names(.df) %in% drop)
     if (length(.w) > 0) .df <- .df[, .w, drop=FALSE]
+    .cls <- class(.df)
     class(.ret) <- "nlmixrFitCoreSilent"
     attr(.cls, ".foceiEnv") <- .ret
     class(.df) <- .cls
