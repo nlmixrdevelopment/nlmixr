4af6a7ebab83d7de3e448bf80103372f52015107
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2020-12-14 18:52:39 -0600

    Be more verbose in testthat to track-down future seg faults

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,4 +3,7 @@ library(RxODE)
 library(nlmixr)
 verbose_minimization <- FALSE
 
-test_check("nlmixr")
+test_check("nlmixr", stop_on_failure = FALSE, wrap=TRUE,
+           reporter = testthat::LocationReporter)
+
+
