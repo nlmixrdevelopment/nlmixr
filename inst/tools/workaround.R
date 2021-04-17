29d42ad79b4e6881f3b5eb51b0bfd831db36f60e
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-04-16 20:47:55 -0500

    Clear RxODE cache with new version of nlmixr too

--- inst/tools/workaround.R
+++ inst/tools/workaround.R
@@ -9,6 +9,19 @@ for (f in c("src/RcppExports.cpp")) {
   }
 }
 
+unlink("R/nlmixr_md5.R")
+
+cpp <- list.files("src", pattern = ".(c|h|cpp|f)$")
+include <- list.files("inst/include")
+Rfiles <- list.files("R/", pattern = ".R")
+md5 <- digest::digest(lapply(c(paste0("src/", cpp),
+                               paste0("inst/include/", include),
+                               paste0("R/", Rfiles)), digest::digest, file = TRUE))
+
+md5file <- file("R/nlmixr_md5.R", "wb")
+writeLines(sprintf("nlmixr.md5 <- \"%s\"\n", md5), md5file)
+close(md5file)
+
 if (.Platform$OS.type == "windows" && !file.exists("src/Makevars.win")) {
   writeLines(gsub("@ISYSTEM@", "I",
                   gsub("@CXX14STD@", "CXX14STD = -std=c++1y",
